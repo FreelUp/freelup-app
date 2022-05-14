a53ef4423863d08db3bc4b52d97048ff47b20e62
Alison Moura <alison.oghino@gmail.com>
2022-05-14 17:48:48 -0400

    Adicionando form na tela de Login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,17 +1,31 @@
 <template>
-  <app-logo></app-logo>
-  <div class="d-flex justify-center">
-    <app-btn block size="x-large"></app-btn>
-  </div>
+  <v-row>
+    <v-col cols="10" offset="1">
+      <app-logo></app-logo>
+      <v-form class="mt-5">
+        <v-row no-gutters>
+          <v-col cols="12">
+            <v-text-field type="email" label="Email" variant="outlined" required></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-text-field type="password" label="Senha" variant="outlined" required></v-text-field>
+          </v-col>
+        </v-row>
+      </v-form>
+      <div class="d-flex justify-center">
+        <app-btn block size="x-large"></app-btn>
+      </div>
+    </v-col>
+  </v-row>
 </template>
 <script>
 import AppLogo from "./../components/AppLogo.vue";
 import AppBtn from "./../components/AppBtn.vue";
 
 export default {
-    components: {
-        AppLogo,
-        AppBtn
-    }
-}
+  components: {
+    AppLogo,
+    AppBtn,
+  },
+};
 </script>
