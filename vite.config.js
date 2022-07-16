39fa66a8b8995d172247270ac4d1b0d129615f06
Alison Moura <alison.oghino@gmail.com>
2022-07-16 17:56:44 -0400

    Adicionando vite-plugin-pwa e ícones auto gerados

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "@vuetify/vite-plugin";
 
@@ -14,6 +15,12 @@ export default defineConfig({
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
     }),
   ],
   define: { "process.env": {} },
